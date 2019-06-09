aea1d534fa0f5a8454f4b29c603e2fd33d11bccc
The Drint <thedrint@yandex.ru>
2019-06-09 11:43:44 +0300

    webpack config for build dev-pixi

---
+++ webpack.dev-pixi.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const wwwData = 'dev-pixi';// Site dir on server (relative to this config)
+const srcData = './src-pixi';// Site dir on server (relative to this config)
+
+module.exports = {
+	mode: "development",
+	devtool:"source-map",
+	devServer: {
+		contentBase: path.join(__dirname, wwwData),
+		publicPath: '/',
+	},
+	entry: {
+		index: `${srcData}/index.js`
+	},
+	output:
+	{
+		path: path.resolve(__dirname, wwwData),
+		filename: '[name].js',
+		publicPath: './',
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin([
+			// {from: './assets/img/decks', to: './assets/img/decks', context: './src'},
+			{from: './assets/locales', to: './assets/locales', context: srcData},
+		]),
+		new HtmlWebpackPlugin({
+			title: 'CastleDef Testing',
+			// template: './src/index.html',
+			favicon: `${srcData}/favicon.ico`,
+		}),
+	],
+	module:
+		{
+			rules:
+				[
+					{
+						test: /\.(png|jpe?g|gif|svg)$/,
+						include: path.resolve(__dirname, `${srcData}/assets/img`),
+						use: [
+							{
+								loader: 'url-loader',
+								options: {
+									context: srcData, name:'[path][name].[ext]',
+									limit: 128,
+								}
+							},
+						],
+					},
+				]
+		},
+};
